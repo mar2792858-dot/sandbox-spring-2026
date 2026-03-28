b455a6c51bcf4583f19b5714ea2ec81ae43fc27d
Maria Hamilton <ai.maria.hamilton@gmail.com>
2026-03-27 21:01:33 -0700

    built out interface LinkedList and added methods to SinglyLinkedList

--- src/main/java/org/example/sandbox/linkedlist/LinkedList.java
+++ src/main/java/org/example/sandbox/linkedlist/LinkedList.java
@@ -5,4 +5,14 @@ public interface LinkedList<E> {
     void addFirst(E element);
     void addLast(E element);
 
+    E pollFirst();
+    E pollLast();
+
+    E peekFirst();
+    E peekLast();
+
+    void clear();
+    boolean contains(E element);
+    int size();
+
 }
--- src/main/java/org/example/sandbox/linkedlist/SinglyLinkedList.java
+++ src/main/java/org/example/sandbox/linkedlist/SinglyLinkedList.java
@@ -1,6 +1,81 @@
 package org.example.sandbox.linkedlist;
 
-public class SinglyLinkedList implements LinkedList{
+import java.util.StringJoiner;
 
+public class SinglyLinkedList<E> implements LinkedList<E>{
 
+    private Node head;
+    private Node tail;
+
+    @Override
+    public void addFirst(E element) {
+
+    }
+
+    @Override
+    public void addLast(E element) {
+
+    }
+
+    @Override
+    public E pollFirst() {
+        return null;
+    }
+
+    @Override
+    public E pollLast() {
+        return null;
+    }
+
+    @Override
+    public E peekFirst() {
+        return null;
+    }
+
+    @Override
+    public E peekLast() {
+        return null;
+    }
+
+    @Override
+    public void clear() {
+
+    }
+
+    @Override
+    public boolean contains(E element) {
+        return false;
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public String toString() {
+
+        StringBuilder builder = new StringBuilder("[");
+        StringJoiner joiner = new StringJoiner(", ");
+
+        Node current = head;
+        if (current != null) {
+
+            // Walking the list loop - pattern will be used in other methods
+            while (current != null) {
+                joiner.add(current.element.toString());
+                current = current.next;
+            }
+        }
+
+        builder.append(joiner);
+        builder.append("]";
+        return builder.toString();
+    }
+
+    private class Node {
+
+        E element:
+        Node next;
+    }
 }
